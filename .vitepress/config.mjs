ec688f466b9b63df0e67d7dbb2098d5a56600656
Max Bokhan <bokhanworks@gmail.com>
2024-09-25 12:52:40 +0300

    favicons

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -7,6 +7,20 @@ export default defineConfig({
   description: "Блог про веб-розробку",
   srcDir: './src',
   outDir: './docs',
+  head: [
+	  ['link', { rel: 'shortcut icon', href: '/favicons/favicon.ico' }],
+    ['link', { rel: 'apple-touch-icon', sizes: '180x180', href: '/favicons/apple-touch-icon.png' }],
+    ['link', { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicons/favicon-32x32.png' }],
+    ['link', { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicons/favicon-16x16.png' }],
+    ['link', { rel: 'manifest', href: '/favicons/site.webmanifest' }],
+    ['link', { rel: 'mask-icon', href: '/favicons/safari-pinned-tab', color: '#5bbad5' }],
+    ['meta', { name: 'apple-mobile-web-app-title', content: 'Neuroweb' }],
+    ['meta', { name: 'application-name', content: 'Neuroweb' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#2d89ef' }],
+    ['meta', { name: 'msapplication-config', content: '/favicons/browserconfig.xml' }],
+    ['meta', { name: 'theme-color', content: '#ffffff' }]
+  ],
+
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
